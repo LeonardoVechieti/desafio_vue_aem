ad25345d2a72fbfb40f11fe2d67df7c9e788a09a
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-25 20:07:32 -0300

    Feat: Creating text component

---
+++ ui.frontend/src/components/micro/MyText/MyText.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="textContainer">
+    <p type="text" :class="textClass">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "MyText",
+  props: {
+    msg: {
+      type: String,
+      default: 'null'
+    },
+    textClass: {
+      type: String,
+      default: 'textClass'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scooped>
+@import "./MyText.scss";
+</style>
