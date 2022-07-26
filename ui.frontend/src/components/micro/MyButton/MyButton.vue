9d206ad8a3c541ebed03e41b9f14310d494365b5
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-25 22:33:21 -0300

    Feat: Creating button component

---
+++ ui.frontend/src/components/micro/MyButton/MyButton.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="containerButton">
+    <button :class="classButton" @click.prevent="event">{{ text }}</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'MyButton',
+  props: {
+    text: {
+      type: String,
+      default: 'defaut'
+    },
+    classButton: {
+      type: String
+    }
+  },
+  methods: {
+    event () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './MyButton.scss';
+</style>
